1a555e6e6afba6ac0e64d1606cfcab574b422815
Rakibur Rahman <Rakibur Rahman@DESKTOP-2L0V4JH>
2023-12-05 02:09:21 +0600

    dashboard update

--- src/library/management/system/AdmindashboardFXMLController.java
+++ src/library/management/system/AdmindashboardFXMLController.java
@@ -4,8 +4,21 @@ import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
 
 public class AdmindashboardFXMLController implements Initializable {
+
+    @FXML
+    private TableColumn<?, ?> colid;
+    @FXML
+    private TableColumn<?, ?> coltittle;
+    @FXML
+    private TableColumn<?, ?> colaurther;
+    @FXML
+    private TableColumn<?, ?> colyear;
+    @FXML
+    private TableColumn<?, ?> colpage;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
--- src/library/management/system/StudentdashboardFXMLController.java
+++ src/library/management/system/StudentdashboardFXMLController.java
@@ -4,8 +4,32 @@ import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
 public class StudentdashboardFXMLController implements Initializable {
+
+    @FXML
+    private TableColumn<?, ?> colid;
+    @FXML
+    private TableColumn<?, ?> coltittle;
+    @FXML
+    private TableColumn<?, ?> colaurther;
+    @FXML
+    private TableColumn<?, ?> colyear;
+    @FXML
+    private TableColumn<?, ?> colpage;
+    @FXML
+    private TextField tfid;
+    @FXML
+    private TextField tftitle;
+    @FXML
+    private TextField tfaurther;
+    @FXML
+    private TextField tfyear;
+    @FXML
+    private TextField tfpage;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
