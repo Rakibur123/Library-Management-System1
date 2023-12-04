9ec5b0758ca79684bec712fe2f76643cb2fd448a
Rakibur Rahman <Rakibur Rahman@DESKTOP-2L0V4JH>
2023-12-04 20:11:33 +0600

    log in page student admin new added

--- src/library/management/system/FXMLDocumentController.java
+++ src/library/management/system/FXMLDocumentController.java
@@ -2,6 +2,10 @@ package library.management.system;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -10,54 +14,114 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class FXMLDocumentController {
 
     @FXML
-    private AnchorPane Anchorebackground1;
-    @FXML
-    private AnchorPane Anchorpane;
-    @FXML
-    private Label Label;
-    @FXML
     private ComboBox<String> combBox;
+
     @FXML
     private TextField username;
+
     @FXML
     private PasswordField password;
+
+    @FXML
+    private AnchorPane Anchorebackground1;
+
+    @FXML
+    private AnchorPane Anchorpane;
+
+    @FXML
+    private Label Label;
+
     @FXML
     private Button signin;
+
     @FXML
     private Button close;
 
     public void initialize() {
-        // Initialize the ComboBox items
         combBox.getItems().addAll("Admin", "Student");
-        combBox.getSelectionModel().selectFirst(); // Set default selection
-    }
-
-    @FXML
-    private void handlepassword(ActionEvent event) {
-        // Handle password field action if needed
+        combBox.getSelectionModel().selectFirst();
     }
 
     @FXML
     private void btnsignin(ActionEvent event) {
-        // Handle sign-in button action
         String userType = combBox.getValue(); // Get the selected user type
+        String enteredPassword = password.getText(); // Get the entered password
 
-        if ("Admin".equals(userType)) {
-            // Code for admin login
+        System.out.println("User Type: " + userType);
+        System.out.println("Entered Password: " + enteredPassword);
+
+        // Check the entered password against the expected passwords for admin and student
+        if ("Admin".equals(userType) && "admin".equals(enteredPassword)) {
             System.out.println("Admin login");
-        } else if ("Student".equals(userType)) {
-            // Code for student login
+            openadmindashboardFXML();
+        } else if ("Student".equals(userType) && "1234".equals(enteredPassword)) {
             System.out.println("Student login");
+            openstudentdashboardFXML();
+        } else {
+            // Incorrect password, show an error message
+            showAlert("Incorrect Password", "Invalid password for " + userType);
         }
+    }
+
+    private void openadmindashboardFXML() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("admindashboardFXML.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Admin Dashboard");
+            stage.show();
+
+            // Close the current login window if needed
+            Stage currentStage = (Stage) username.getScene().getWindow();
+            currentStage.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void openstudentdashboardFXML() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("studentdashboardFXML.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Student Dashboard");
+            stage.show();
+
+            // Close the current login window if needed
+            Stage currentStage = (Stage) username.getScene().getWindow();
+            currentStage.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 
     @FXML
     private void btnclose(ActionEvent event) {
         // Handle close button action if needed
-        Stage stage = (Stage) close.getScene().getWindow();
+        Stage stage = (Stage) username.getScene().getWindow();
         stage.close();
+    }
+
+    @FXML
+    private void handlepassword(ActionEvent event) {
+        // Handle password field action if needed
+        System.out.println("Password field action");
     }
 }
