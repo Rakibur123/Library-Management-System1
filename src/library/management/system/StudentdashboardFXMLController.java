872e4f84907f5e73808d0ed28125d61bf625056d
Rakibur Rahman <Rakibur Rahman@DESKTOP-2L0V4JH>
2023-12-05 02:11:43 +0600

    dashboard update now

--- src/library/management/system/StudentdashboardFXMLController.java
+++ src/library/management/system/StudentdashboardFXMLController.java
@@ -7,7 +7,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
-
+ 
 public class StudentdashboardFXMLController implements Initializable {
 
     @FXML
