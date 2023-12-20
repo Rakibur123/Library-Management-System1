69d5a602988f43971535c1449101bbb5264f20f7
Rakibur Rahman <Rakibur Rahman@DESKTOP-2L0V4JH>
2023-12-20 11:02:25 +0600

    update logout minimize button

--- src/library/management/system/FXMLDocumentController.java
+++ src/library/management/system/FXMLDocumentController.java
@@ -1,5 +1,6 @@
 package library.management.system;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,101 +9,117 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class FXMLDocumentController {
-
-    @FXML
-    private ComboBox<String> combBox;
-
-    @FXML
-    private TextField username;
-
-    @FXML
-    private PasswordField password;
+public class FXMLDocumentController implements Initializable {
 
     @FXML
     private AnchorPane Anchorebackground1;
-
     @FXML
     private AnchorPane Anchorpane;
-
-    @FXML
-    private Label Label;
-
-    @FXML
-    private Button signin;
-
     @FXML
     private Button close;
+    @FXML
+    private Button lognin;
+    @FXML
+    private FontAwesomeIconView users;
+    @FXML
+    private Button minimize;
+    @FXML
+    private PasswordField Password;
+    @FXML
+    private TextField studeNumer;
+    @FXML
+    private Label Label;
+    @FXML
+    private ComboBox<String> combobox;
 
-    public void initialize() {
-        combBox.getItems().addAll("Admin", "Student");
-        combBox.getSelectionModel().selectFirst();
+    private Connection connect;
+    private PreparedStatement prepare;
+    private ResultSet result;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        combobox.setItems(FXCollections.observableArrayList("Admin", "Student"));
     }
 
     @FXML
-    private void btnsignin(ActionEvent event) {
-        String userType = combBox.getValue(); // Get the selected user type
-        String enteredPassword = password.getText(); // Get the entered password
+    private void btnlognin(ActionEvent event) {
+        String studentSql = "SELECT * FROM student WHERE studentNumber = ? AND password = ?";
+        String adminSql = "SELECT * FROM admin WHERE adminNumber = ? AND password = ?";
 
-        System.out.println("User Type: " + userType);
-        System.out.println("Entered Password: " + enteredPassword);
+        try {
+            connect = DriverManager.getConnection("jdbc:mysql://localhost/librarydatabase", "root", "");
 
-        // Check the entered password against the expected passwords for admin and student
-        if ("Admin".equals(userType) && "admin".equals(enteredPassword)) {
-            System.out.println("Admin login");
-            openadmindashboardFXML();
-        } else if ("Student".equals(userType) && "1234".equals(enteredPassword)) {
-            System.out.println("Student login");
-            openstudentdashboardFXML();
+            prepare = connect.prepareStatement(combobox.getValue().equals("Student") ? studentSql : adminSql);
+            prepare.setString(1, studeNumer.getText());
+            prepare.setString(2, Password.getText());
+            result = prepare.executeQuery();
+
+            if (studeNumer.getText().isEmpty() || Password.getText().isEmpty()) {
+                // Show an alert for empty fields
+                showAlert("Error", "Please enter both student/admin number and password.");
+            } else {
+                // Check login credentials
+                if (result.next()) {
+                    // Login successful, open dashboard based on the selected role
+                    String selectedRole = combobox.getValue();
+                    openDashboard(selectedRole);
+                } else {
+                    // Invalid credentials, show an error message
+                    showAlert("Error", "Invalid credentials. Please try again.");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Close resources in the finally block
+            closeResources();
+        }
+    }
+
+    private void password(KeyEvent event) {
+        if (!event.getCharacter().matches("[\\d]")) {
+            event.consume();
+            studeNumer.setStyle("-fx-border-color:#e04040");
         } else {
-            // Incorrect password, show an error message
-            showAlert("Incorrect Password", "Invalid password for " + userType);
+            studeNumer.setStyle("-fx-border-color:#fff");
         }
     }
 
-    private void openadmindashboardFXML() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("admindashboardFXML.fxml"));
-            Parent root = loader.load();
+   @FXML
+private void btnclose(ActionEvent event) {
+    // Get the reference to the current stage
+    Stage stage = (Stage) close.getScene().getWindow();
 
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root));
-            stage.setTitle("Admin Dashboard");
-            stage.show();
+    // Close the current stage
+    stage.close();
+}
 
-            // Close the current login window if needed
-            Stage currentStage = (Stage) username.getScene().getWindow();
-            currentStage.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+@FXML
+private void btnminimize(ActionEvent event) {
+    // Get the reference to the current stage
+    Stage stage = (Stage) minimize.getScene().getWindow();
 
-    private void openstudentdashboardFXML() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("studentdashboardFXML.fxml"));
-            Parent root = loader.load();
-
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root));
-            stage.setTitle("Student Dashboard");
-            stage.show();
-
-            // Close the current login window if needed
-            Stage currentStage = (Stage) username.getScene().getWindow();
-            currentStage.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+    // Minimize the current stage
+    stage.setIconified(true);
+}
 
     private void showAlert(String title, String content) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -112,16 +129,47 @@ public class FXMLDocumentController {
         alert.showAndWait();
     }
 
-    @FXML
-    private void btnclose(ActionEvent event) {
-        // Handle close button action if needed
-        Stage stage = (Stage) username.getScene().getWindow();
-        stage.close();
+    private void closeResources() {
+        try {
+            if (result != null) {
+                result.close();
+            }
+            if (prepare != null) {
+                prepare.close();
+            }
+            if (connect != null) {
+                connect.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
-    @FXML
-    private void handlepassword(ActionEvent event) {
-        // Handle password field action if needed
-        System.out.println("Password field action");
+    private void openDashboard(String role) {
+        String dashboardFXML;
+        if ("Student".equals(role)) {
+            dashboardFXML = "StudentDashboard.fxml";
+        } else if ("Admin".equals(role)) {
+            dashboardFXML = "AdminDashboard.fxml";
+        } else {
+            showAlert("Error", "Invalid role selected.");
+            return;
+        }
+
+        try {
+            // Load the corresponding dashboard FXML file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(dashboardFXML));
+            Parent root = loader.load();
+
+            // Create a new scene
+            Scene scene = new Scene(root);
+
+            // Get the current stage and set the new scene
+            Stage stage = (Stage) lognin.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
